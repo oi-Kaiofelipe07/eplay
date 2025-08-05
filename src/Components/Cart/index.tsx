3f459f0832cf53a82a26e69eda3b72d9ae44d47f
Kaio Felipe <kaiofelipee12@gmail.com>
2025-08-05 15:53:08 -0300

    Adicione e remova produtos

--- src/Components/Cart/index.tsx
+++ src/Components/Cart/index.tsx
@@ -8,14 +8,14 @@ import {
   Prices,
   Sidebar
 } from './styles'
-import starWars from '../../assets/images/star_wars.png'
 import Tag from '../Tag'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootReducer } from '../../store'
-import { close } from '../../store/reducers/Cart'
+import { close, remove } from '../../store/reducers/Cart'
+import { formataPreco } from '../productsList'
 
 const Cart = () => {
-  const { isOpen } = useSelector((state: RootReducer) => state.cart)
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
 
   const dispatch = useDispatch()
 
@@ -23,39 +23,41 @@ const Cart = () => {
     dispatch(close())
   }
 
+  const getTotalPrice = () => {
+    return items.reduce((acumulador, valorAtual) => {
+      return (acumulador += valorAtual.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
   return (
     <CartContainer className={isOpen ? 'is-open' : ''}>
       <Overlay onClick={closeCart} />
       <Sidebar>
         <ul>
-          <CartItem>
-            <img src={starWars} alt="" />
-            <div>
-              <h3>Nome do jogo</h3>
-              <Tag>RPG</Tag>
-              <Tag>PS5</Tag>
-              <span>R$ 150,00</span>
-            </div>
-            <button type="button" />
-          </CartItem>
-          <CartItem>
-            <img src={starWars} alt="" />
-            <div>
-              <h3>Nome do jogo</h3>
-              <Tag>RPG</Tag>
-              <Tag>PS5</Tag>
-              <span>R$ 150,00</span>
-            </div>
-            <button type="button" />
-          </CartItem>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.thumbnail} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{formataPreco(item.prices.current)}</span>
+              </div>
+              <button onClick={() => removeItem(item.id)} type="button">
+                Remover
+              </button>
+            </CartItem>
+          ))}
         </ul>
-        <Quantity>2 jogo(s) no carrinho</Quantity>
+        <Quantity>{items.length} jogo(s) no carrinho</Quantity>
         <Prices>
-          Total de R$ 250,00 <span>Em até 6x sem juros</span>
+          Total de {formataPreco(getTotalPrice())}{' '}
+          <span>Em até 6x sem juros</span>
         </Prices>
-        <Button title="Clique aqui para continuar com a compra" type="button">
-          Continuar com a compra
-        </Button>
       </Sidebar>
     </CartContainer>
   )
--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -4,10 +4,12 @@ import { HeaderBar, LinkItem, Links, CartButton } from './styles'
 import logo from '../../assets/images/logo.svg'
 import carrinho from '../../assets/images/carrinho.svg'
 import { open } from '../../store/reducers/Cart'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
 
 const Header = () => {
   const dispatch = useDispatch()
+  const { items } = useSelector((state: RootReducer) => state.cart)
 
   const openCart = () => {
     dispatch(open())
@@ -34,7 +36,7 @@ const Header = () => {
         </nav>
       </div>
       <CartButton onClick={openCart} href="#">
-        0 - produto(s)
+        <span>{items.length}</span>
         <img src={carrinho} alt="Carrinho" />
       </CartButton>
     </HeaderBar>
